d4d22662f9a212f50a77e44557ef7d1e5ce957ff
Marco <marcoerhardt95@gmail.com>
2021-02-02 15:31:22 +0100

    fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,5 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'   
   get 'welcome', to: 'sessions#welcome'
   delete 'sign_out' => 'sessions#destroy'
-  post 'event_attendances/create' => 'event_attendances'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
